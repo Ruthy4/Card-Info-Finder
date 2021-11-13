2e0e1d06b31c11541ec2599481d177fe5907c819
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2021-11-13 18:27:57 +0100

    ui test implementation

--- app/src/androidTest/java/com/example/cardinfofinder/ui/CardInfoActivityTest.kt
+++ app/src/androidTest/java/com/example/cardinfofinder/ui/CardInfoActivityTest.kt
@@ -1,17 +1,25 @@
 package com.example.cardinfofinder.ui
 
+import android.app.Dialog
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.cardinfofinder.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.test.ext.junit.rules.activityScenarioRule
+import java.lang.StringBuilder
 
 
 @RunWith(AndroidJUnit4::class)
@@ -25,6 +33,8 @@ class CardInfoActivityTest {
     var activityScenarioRule: ActivityScenarioRule<CardInfoActivity> =
         ActivityScenarioRule(CardInfoActivity::class.java)
 
+    private var dialog: BottomSheetDialog? = null
+
     @Before
     fun setUp() {
         hiltRule.inject()
@@ -36,5 +46,17 @@ class CardInfoActivityTest {
         Espresso.onView(ViewMatchers.withId(R.id.card_number_edit_text))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
+
+    @Test
+    fun test_that_user_can_enter_input() {
+        Espresso.onView(ViewMatchers.withId(R.id.card_number_edit_text)).perform(typeText("41874515"))
+    }
+
+    @Test
+    fun test_button_visibility_name(){
+        Espresso.onView(ViewMatchers.withId(R.id.check_button))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
 }
 
